c776aee239694f145c297d39603282efc35fc135
Bernal03 <61256617+Bernal03@users.noreply.github.com>
2020-10-27 15:33:23 -0700

    Updates

---
+++ precache-manifest.85ca56561c029b8f3d31e8ccb6fee93b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b153a118a8d0f81da78af7af804be4f2",
+    "url": "/ProyectoReact/index.html"
+  },
+  {
+    "revision": "1fa3cd7000181cef8e74",
+    "url": "/ProyectoReact/static/css/main.bd129949.chunk.css"
+  },
+  {
+    "revision": "f20d3b19aa71657a2ea0",
+    "url": "/ProyectoReact/static/js/2.f7a97581.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ProyectoReact/static/js/2.f7a97581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fa3cd7000181cef8e74",
+    "url": "/ProyectoReact/static/js/main.29cf6e2b.chunk.js"
+  },
+  {
+    "revision": "e35ac71442671c3b351e",
+    "url": "/ProyectoReact/static/js/runtime-main.44057c49.js"
+  },
+  {
+    "revision": "96aff77c5833d91511a87f051dc63231",
+    "url": "/ProyectoReact/static/media/logoTec.96aff77c.jpg"
+  }
+]);
